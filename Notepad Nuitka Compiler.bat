893ad48b92e0fffe99b19e9fdbf0caec0feea780
cr-623 <chiren623@gmail.com>
2026-03-08 22:42:54 +0800

    Update Notepad Nuitka Compiler.bat

--- Notepad Nuitka Compiler.bat
+++ Notepad Nuitka Compiler.bat
@@ -1,4 +1,5 @@
 @echo off
-REM Requires nuitka installed and notepad.ico in the same directory
-python -m nuitka --onefile --enable-plugin=pyside6 --windows-icon-from-ico=notepad.ico --windows-console-mode=disable --include-data-files=notepad.ico=notepad.ico --output-filename=Notepad+++++ --lto=yes "Notepad Clone - PySide6 edition.pyw"
+REM Requires nuitka and pyspellchecker installed and notepad.ico in the same directory
+python -m nuitka --onefile --enable-plugin=pyside6 --windows-icon-from-ico=notepad.ico --windows-console-mode=disable --include-data-files=notepad.ico=notepad.ico --include-package-data=spellchecker --output-filename=Notepad++++ --lto=yes "Notepad Clone - PySide6 edition.pyw"
 pause
+
